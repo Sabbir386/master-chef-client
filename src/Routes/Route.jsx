b0bbe34cd93decaeca0742bb8a96dffaa1a130fb
Sabbir386 <sabbir386.sa@gmail.com>
2023-05-02 17:13:31 +0600

    added login and registarion form

--- src/Routes/Route.jsx
+++ src/Routes/Route.jsx
@@ -3,6 +3,8 @@ import { createBrowserRouter } from "react-router-dom";
 import Main from "../Main/Main";
 import ErrorPage from "../components/ErrorPage/ErrorPage";
 import Home from "../Home/Home";
+import Login from "../pages/Login/Login/Login";
+import Register from "../pages/Login/Register/Register";
 
 const router = createBrowserRouter([
     {
@@ -13,7 +15,15 @@ const router = createBrowserRouter([
             {
                 path: '/',
                 element: <Home></Home>,
-            }
+            },
+            {
+                path: '/register',
+                element: <Register></Register>
+            },
+            {
+                path: '/login',
+                element: <Login />,
+            },
         ]
 
     },
