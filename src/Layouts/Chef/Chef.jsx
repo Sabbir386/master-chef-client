f7d31773bfc8c440aed659c47104a03d937877e0
Sabbir386 <sabbir386.sa@gmail.com>
2023-05-02 20:30:36 +0600

    got data from api

--- src/Layouts/Chef/Chef.jsx
+++ src/Layouts/Chef/Chef.jsx
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import ChefMaster from './ChefMatser/ChefMaster';
 
 const Chef = () => {
 
-    return (
+    const [chefMaster, setChefMaster] = useState([]);
 
-        <h2>chef page</h2>
+    useEffect(() => {
+        fetch('http://localhost:5000/categories')
+            .then(res => res.json())
+            .then(data => setChefMaster(data))
+            .catch(error => console.log(error))
+    }, [])
+    return (
+        <div>
+            <div className='text-center text-3xl font-semibold'>Our Chef Masters</div>
+            {
+                chefMaster.map(data => <ChefMaster
+                    key={data.id}
+                    data={data}
+                >
+                    {/* <Link to={`/data/${data.id}`} className='text-black'>{data.name}</Link> */}
+
+                </ChefMaster>)
+            }
+
+        </div>
+
+
+
 
     );
 };
--- src/pages/Login/Login/Login.jsx
+++ src/pages/Login/Login/Login.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 const Login = () => {
+
     return (
         <section class="bg-gray-50 dark:bg-gray-900">
             <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
