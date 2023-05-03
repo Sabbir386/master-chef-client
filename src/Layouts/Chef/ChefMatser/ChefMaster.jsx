808999747af2601251285a994cae96a2b7d5a7b0
Sabbir386 <sabbir386.sa@gmail.com>
2023-05-03 16:34:06 +0600

    added chef details page banner section

--- src/Layouts/Chef/ChefMatser/ChefMaster.jsx
+++ src/Layouts/Chef/ChefMatser/ChefMaster.jsx
@@ -2,21 +2,27 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const ChefMaster = ({ data }) => {
-    const { id, name, experience, recipes, likes } = data;
+    const { id, name, years_of_experience, number_of_recipes, likes, picture } = data;
 
     return (
 
         <div className="card w-96 bg-base-100 shadow-xl">
             <figure className="px-10 pt-10">
-                <img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" className="rounded-xl" />
+                <img src={picture} alt="Shoes" className="rounded-xl h-28 object-cover" />
             </figure>
             <div className="card-body items-center text-center">
                 <h2 className="card-title">{name}</h2>
-                <p>If a dog chews shoes whose shoes does he choose?</p>
+                <h3>  Number of Experience :  {years_of_experience}</h3>
+                <h3> Number of Recipes : {number_of_recipes}</h3>
+                <h3> Peoples Likes Recipes : {likes}</h3>
                 <div className="card-actions">
-                    <button className="btn btn-primary">
-                        <Link to={`/recepies/${id}`} className='text-black'>View Recipes</Link>
-                    </button>
+                    {/* <button className="btn btn-primary">
+                        <Link to={`/recepies/${id}`} className='text-white'>View Recipes</Link>
+                    </button> */}
+
+                    <Link to={`/recepies/${id}`} className='text-white'><button className="btn btn-primary">
+                        View Recipes
+                    </button></Link>
 
                 </div>
             </div>
--- src/Layouts/Chef/ChefRecipes.jsx
+++ src/Layouts/Chef/ChefRecipes.jsx
@@ -5,9 +5,62 @@ const ChefRecipes = () => {
     const { id } = useParams();
     const ChefDeatils = useLoaderData();
     console.log(ChefDeatils);
+
+    const { picture, bio, likes, years_of_experience, number_of_recipes, name } = ChefDeatils;
     return (
-        <div>
-            <h2>chef recepes page</h2>
+        <div className=' w-50 px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 '>
+            <div className='px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8'>
+                <div className="card lg:card-side bg-base-100 shadow-xl items-center justify-center">
+                    <figure><img className='h-80' src={picture} alt="Album" /></figure>
+                    <div className="card-body">
+                        <h2 className="card-title">{name}</h2>
+                        <p>{bio}</p>
+                        <h3>  Number of Experience :  {years_of_experience}</h3>
+                        <h3> Number of Recipes : {number_of_recipes}</h3>
+                        <h3> Peoples Likes Recipes : {likes}</h3>
+                        <div className="card-actions justify-end">
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+
+            <div className=' px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8'>
+                <div class="flex flex-wrap">
+                    <div class="w-full md:w-1/3 lg:px-4">
+                        <div class="bg-white rounded-lg shadow-lg mb-4">
+                            <img src="https://via.placeholder.com/640x480.png/0099CC?text=Card+Image" alt="Card Image" class="w-full rounded-t-lg" />
+                            <div class="p-4">
+                                <h3 class="text-lg font-medium mb-2">Card Title</h3>
+                                <p class="text-gray-600">Card Description</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="w-full md:w-1/3 lg:px-4">
+                        <div class="bg-white rounded-lg shadow-lg mb-4">
+                            <img src="https://via.placeholder.com/640x480.png/0099CC?text=Card+Image" alt="Card Image" class="w-full rounded-t-lg" />
+                            <div class="p-4">
+                                <h3 class="text-lg font-medium mb-2">Card Title</h3>
+                                <p class="text-gray-600">Card Description</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="w-full md:w-1/3 lg:px-4">
+                        <div class="bg-white rounded-lg shadow-lg mb-4">
+                            <img src="https://via.placeholder.com/640x480.png/0099CC?text=Card+Image" alt="Card Image" class="w-full rounded-t-lg" />
+                            <div class="p-4">
+                                <h3 class="text-lg font-medium mb-2">Card Title</h3>
+                                <p class="text-gray-600">Card Description</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+
 
         </div>
     );
